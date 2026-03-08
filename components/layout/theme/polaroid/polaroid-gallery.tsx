ca098220d50fb0ba41412214cbfe3363b58db8e9
Box <me@boxz.dev>
2026-03-09 00:12:12 +0800

    update

--- components/layout/theme/polaroid/polaroid-gallery.tsx
+++ components/layout/theme/polaroid/polaroid-gallery.tsx
@@ -180,7 +180,7 @@ export default function PolaroidGallery(props: Readonly<ImageHandleProps>) {
   return (
     <DraggableCardContainer className="relative flex min-h-screen w-full items-center justify-center overflow-clip">
       <p className="absolute top-1/2 mx-auto max-w-sm -translate-y-3/4 text-center text-2xl font-black text-neutral-400 md:text-4xl dark:text-neutral-800">
-        {customTitle || '瓦达西可不可爱'}
+        {customTitle || '大福映画 Felina Gallery'}
       </p>
       {dataList?.map((item: ImageType) => (
         <PolaroidCard
